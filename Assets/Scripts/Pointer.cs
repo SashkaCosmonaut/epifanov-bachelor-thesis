a158401c9bb7d1fac46c17c0beb8697629901f1b
SashkaCosmonaut <sashkacosmonaut@gmail.com>
2020-04-17 12:58:14 +0400

    Добавлены классы указателя и модуля ввода.

---
+++ Assets/Scripts/Pointer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Класс указателя на объекты на сцене.
+    /// </summary>
+    public class Pointer : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/VRInputModule.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// Класс обработки событий взаимодействия с окружением.
+    /// </summary>
+    public class VRInputModule : MonoBehaviour
+    {
+    }
+}
